b3c941d4f58abc8d0d1ee060cdbcca7adeca67a6
mateosaravia <msaravia2710@gmail.com>
2024-01-29 11:54:30 -0300

    Implemented profile repository

--- application/src/controllers/profiles/profile-controller.ts
+++ application/src/controllers/profiles/profile-controller.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 export const router = express.Router();
 
+import * as profileService from '../../services/profiles/profile-service';
+
 router.post('/users/:userId/profiles', async (req, res, next) => {
   try {
-    let profile = profileService.createProfile(req.body);
+    const { userId } = req.params;
+    let profile = profileService.createProfile(parseInt(userId), req.body);
     return res.status(201).send(profile);
   } catch (err) {
     return next(err);
@@ -12,7 +15,9 @@ router.post('/users/:userId/profiles', async (req, res, next) => {
 
 router.patch('/users/:userId/profiles', async (req, res, next) => {
   try {
-    let updatedProfile = profileService.updateProfile(req.body);
+    const { userId } = req.params;
+    let updatedProfile = profileService.updateProfile(parseInt(userId), req.body);
+    return res.status(200).send(updatedProfile);
   } catch (err) {
     return next(err);
   }
@@ -21,7 +26,7 @@ router.patch('/users/:userId/profiles', async (req, res, next) => {
 router.get('/users/:userId/profiles', async (req, res, next) => {
   try {
     const { userId } = req.params;
-    let profile = profileService.getProfileById(userId);
+    let profile = profileService.getProfileById(parseInt(userId));
     return res.status(200).send(profile);
   } catch (err) {
     return next(err);
--- application/src/services/profiles/profile-service.ts
+++ application/src/services/profiles/profile-service.ts
@@ -1,10 +1,11 @@
 import * as exceptions from '../../common/exceptions/exceptions';
 import * as userService from '../users/user-service';
+import * as profileRepository from '../../data-access/repositories/profiles/profile-repository';
 
 import { validateProfile } from './profile-validator';
 import { ProfileInput, ProfileOutput } from '../../data-access/models/profiles/profile-model';
 
-export const createProfile = async (userId: number, newProfile: ProfileInput): ProfileOutput => {
+export const createProfile = async (userId: number, newProfile: ProfileInput): Promise<ProfileOutput> => {
   validateProfile(newProfile);
 
   let user = await userService.getUserById(userId);
@@ -21,7 +22,7 @@ export const createProfile = async (userId: number, newProfile: ProfileInput): P
   return await profileRepository.createProfile(newProfile);
 };
 
-export const updateProfile = async (userId: number, updatedProfile: ProfileInput): ProfileOutput => {
+export const updateProfile = async (userId: number, updatedProfile: ProfileInput): Promise<ProfileOutput> => {
   validateProfile(updatedProfile);
 
   let existsProfile: boolean = await existsProfileByUserId(userId);
@@ -32,12 +33,12 @@ export const updateProfile = async (userId: number, updatedProfile: ProfileInput
   return await profileRepository.updateProfile(userId, updatedProfile);
 };
 
-export const getProfileById = async (userId: number): ProfileOutput | null => {
-  const profile = await profileRepository.getProfileById(userId);
+export const getProfileById = async (userId: number): Promise<ProfileOutput | null> => {
+  const profile = await profileRepository.getProfileByUserId(userId);
   return profile;
 };
 
 export const existsProfileByUserId = async (userId: number): Promise<boolean> => {
-  const profile = await profileRepository.getProfileById(userId);
+  const profile = await profileRepository.getProfileByUserId(userId);
   return !!profile;
 };
