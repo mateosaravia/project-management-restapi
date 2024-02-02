7be7dfe8291ebda6ce332bb5b163abccf8aaf701
mateosaravia <msaravia2710@gmail.com>
2024-02-02 16:07:48 -0300

    Implemented auth middleware

---
+++ application/src/common/middlewares/auth-middlware.ts
@@ -0,0 +1,33 @@
+import { verifyJWT } from '../../services/tokens/token-validator';
+import { evalException } from '../exceptions/exceptions';
+
+export const verifyToken = async (req: any, res: any, next: any) => {
+  const authorization = req.header('Authorization');
+  const token = authorization ? authorization.replace('Bearer ', '') : null;
+
+  try {
+    let decodedToken = await verifyJWT(token);
+    req.userEmail = decodedToken.email;
+    next();
+  } catch (err: any) {
+    return evalException(err, res);
+  }
+};
+
+export const verifyPermissions = (permitedRoles: [String]) => {
+  return async (req: any, res: any, next: any) => {
+    const authorization = req.header('Authorization');
+    const token = authorization ? authorization.replace('Bearer ', '') : null;
+    
+    try {
+      let decodedToken = await verifyJWT(token);
+      if (permitedRoles.includes(decodedToken.role)) {
+        next();
+      } else {
+        return res.status(403).send('User is not authorized to perform this action');
+      }
+    } catch (err: any) {
+      return evalException(err, res);
+    }
+  };
+};
--- application/src/services/tokens/token-validator.ts
+++ application/src/services/tokens/token-validator.ts
@@ -4,26 +4,31 @@ import jwt from 'jsonwebtoken';
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY || 'H+MbQeThWmYq3t6w9z$C&F)J@NcRfUjX';
 
-export const verifyJWT = async function (token: string | null) {
+interface DecodedToken {
+  email: string;
+  userId: number;
+  role: string;
+}
+
+export const verifyJWT = async (token: string | null): Promise<DecodedToken> => {
   token = token ? token.replace('Bearer ', '') : null;
   if (!token) {
     throw new InvalidCredentials('Invalid token');
   }
   await blacklistedToken(token);
-  let decoded = jwt.verify(token, SECRET_KEY, function (err, decodedToken) {
-    if (err) {
-      if (err.name === 'TokenExpiredError') {
-        throw new InvalidCredentials('Token expired');
-      } else {
-        throw new InvalidCredentials('Invalid token');
-      }
+  try {
+    let decoded = jwt.verify(token, SECRET_KEY) as DecodedToken;
+    return decoded;
+  } catch (err: any) {
+    if (err.name === 'TokenExpiredError') {
+      throw new InvalidCredentials('Token expired');
+    } else {
+      throw new InvalidCredentials('Invalid token');
     }
-    return decodedToken;
-  });
-  return decoded;
+  }
 };
 
-export const blacklistedToken = async function (token: string) {
+export const blacklistedToken = async (token: string) => {
   let reply;
   try {
     const redisClient = getClient();
